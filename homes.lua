41d1656c8ce928439e2a949c4d4557160f125863
nouseforname <rookienouse@gmail.com>
2014-11-27 07:05:10 +0100

    Initial commit

---
+++ homes.lua
@@ -0,0 +1,289 @@
+
+-- * plugin homes.lua
+
+
+-- * provides functions to create, request and list and delete player homes with names and configurable limit
+
+-- * permission "homes.home"
+
+-- * Usage /home [[name]|set|list|delete|help] [name]
+-- * /home => port you to your default home
+-- * /home myHome => port you to home 'myHome'
+-- * /home set => set the actual position as default home
+-- * /home set myHome => set the actual position as 'myHome'
+-- * /home list => list all your home by name and world
+-- * /home delete myHome => delete 'myHome"
+-- * /home help => shows this help
+
+
+-- * Configurable limit of homes for each rank. 0=infinite
+-- * Homes.ini:
+-- * [Limits]
+-- * Default=3
+-- * VIP=5
+-- * Operator=10
+-- * Admin=0
+
+
+-- * created by nouseforname @ http://nouseforname.de
+-- * november 2014
+
+
+-- ********************************************************************
+-- ********************************************************************
+
+
+
+-- set plugin folder
+homeDir = 'Plugins/Homes'
+
+-- Plugin name and prefix for later usage
+PluginName = "Homes"
+PluginPrefix = PluginName .. ": "
+
+-- initialize global tables
+g_Config = {}
+g_Storage = {}
+
+
+-- init plugin
+function Initialize(Plugin)
+    
+    PLUGIN = Plugin
+    
+    PLUGIN:SetName(PluginName)
+    PLUGIN:SetVersion(1)
+    
+    PluginManager = cRoot:Get():GetPluginManager()
+    PluginManager:BindCommand("/home", "homes.home", HandleHomeCommand, " - Handle commands like [list|set|delete|help] or teleport!")
+    
+    InitializeConfig()
+    
+    if (not(InitializeStorage())) then
+        LOGWARNING(PluginPrefix .. "failed to initialize Storage, plugin will be disabled");
+        return false;
+    end
+    
+    LOG("Initialized " .. PLUGIN:GetName() .. " v." .. PLUGIN:GetVersion())
+    
+    return true;
+end
+
+
+-- use a config.ini file
+function InitializeConfig()
+    
+    local g_ini = cIniFile()
+    g_ini:ReadFile(PluginName .. ".ini");
+    
+    -- home limits for each ranking, 
+    g_Config.LIMITUSER  = g_ini:GetValueSetI("Limits", "Default", 3);
+    g_Config.LIMITVIP   = g_ini:GetValueSetI("Limits", "VIP", 5);
+    g_Config.LIMITOP    = g_ini:GetValueSetI("Limits", "Operator", 10);
+    g_Config.LIMITADMIN = g_ini:GetValueSetI("Limits", "Admin", 0);
+    
+    g_ini:WriteFile(PluginName .. ".ini");
+    
+end
+
+
+-- handle all commands and call sub functions to execute
+function HandleHomeCommand(args, player)
+    
+    -- get args
+    if #args == 1 then
+        
+        -- move player to default home
+        return moveToHome(player, "default")
+    
+    elseif args[2] == 'list' then
+        
+        if #args >= 3 then
+            player:SendMessage(cChatColor.Red .. 'To many arguments for listing... abort command!!!')
+            return true
+        else 
+            -- list all player homes
+            return listHomes(player)
+        end
+
+    elseif args[2] == 'help' then
+        
+        if #args >= 3 then
+            player:SendMessage(cChatColor.Red .. 'To many arguments for help... abort command!!!')
+            return true
+        else 
+            -- display plugin help
+            return showHelp(player)
+        end
+
+    elseif args[2] == 'set' then
+        
+        if #args >= 4 then
+            player:SendMessage(cChatColor.Red .. 'To many arguments to set home... abort command!!!')
+            return true
+        elseif #args == 3 then
+            -- set given home 
+            return setHome(player, string.gsub(args[3], "%s", ""))
+        else
+            -- set default home
+            return setHome(player, "default")
+        end
+    
+    elseif args[2] == 'delete' then
+        
+        if #args >= 4 then
+            player:SendMessage(cChatColor.Red .. 'To many arguments to delete home... abort command!!!')
+            return true
+        elseif #args == 3 then
+            -- delete given home
+            return deleteHome(player, string.gsub(args[3], "%s", ""))
+        else
+            -- this is not allowed
+            player:SendMessage(cChatColor.Red .. 'Name missing... abort command!!!')
+            return true
+        end
+
+    elseif #args == 2 then
+        -- move player to given home if exist 
+        return moveToHome(player, string.gsub(args[2], "%s", ""))
+    end
+
+    return false
+end
+
+
+-- move player to given home
+function moveToHome(player, sHome)
+
+    local a_Data = g_Storage:GetHome({PLAYER=player:GetName(), NAME=sHome})
+    if a_Data == nil then
+        
+        player:SendMessage(cChatColor.Red .. 'Home doesn\'t exist...')
+        return true
+    end
+    
+    player:SendMessage(cChatColor.Green .. 'Teleporting you to home "' .. a_Data.Name .. '"...')
+    player:TeleportToCoords(a_Data.X, a_Data.Y, a_Data.Z)
+    if player:GetWorld():GetName() ~= a_Data.World then
+        player:MoveToWorld(a_Data.World)
+    end
+    return true
+end
+
+
+-- set given home
+function setHome(player, sHome)
+
+    local a_data = {}
+    a_data.PLAYER   = player:GetName()
+    a_data.NAME     = sHome
+    a_data.WORLD    = player:GetWorld():GetName()
+    a_data.X        = player:GetPosX()
+    a_data.Y        = player:GetPosY()
+    a_data.Z        = player:GetPosZ()
+    a_data.RANK     = "Default"
+    a_data.LIMIT    = g_Config.LIMITUSER
+    
+    -- get PLayerUUID for rank recognition
+    -- local Ranks = cRankManager:GetAllRanks()
+    local PlayerUUID =  cMojangAPI:GetUUIDFromPlayerName(a_data.PLAYER)
+    if ((PlayerUUID ~= nil) and (string.len(PlayerUUID) == 32)) then
+        a_data.RANK = cRankManager:GetPlayerRankName(PlayerUUID)
+        a_data.LIMIT = GetRankLimit(a_data.RANK)
+    end
+
+
+    -- check for permission and existing home
+    local bExists = g_Storage:CheckForExisting(a_data)
+    if not bExists then
+        local count = g_Storage:GetTotalCount(a_data)
+        if a_data.LIMIT ~= 0 and a_data.LIMIT <= count then
+            player:SendMessage(cChatColor.Red .. 'No permission to add more home positions!!!')
+            return true
+        end
+    end
+    
+    
+    -- try to save into database
+    if (not (g_Storage:SetHome( a_data ))) then
+        player:SendMessage(cChatColor.Red .. 'Home NOT set!')
+        return false
+    end
+
+    
+    -- success and message
+    local sMessage = ""
+    if not bExists then
+        sMessage = "New \"" .. a_data.NAME .. "\" home position set!"
+    else
+        sMessage = "Replaced position of \"" .. a_data.NAME .. "\" home!"
+    end
+    player:SendMessage(cChatColor.Green .. sMessage)
+    return true
+end
+
+
+-- list all player homes
+function listHomes(player)
+    
+    local a_List = g_Storage:GetHomeList(player:GetName())
+    player:SendMessage(cChatColor.Green .. "You own " .. #a_List .. " homes!")
+    for i=1, #a_List do
+        local msg = cChatColor.Green .. i .. ". " .. a_List[i].Name .. " @ " .. a_List[i].World
+        player:SendMessage(msg)
+    end
+    return true
+end
+
+
+-- delete given home
+function deleteHome(player, sHome)
+    local res = g_Storage:DeleteHome({PLAYER=player:GetName(), NAME=sHome})
+    if res then
+        player:SendMessage(cChatColor.Green .. "Home " .. sHome .. " successfully deleted!!!")
+    end
+    return res
+end
+
+
+-- display plugin help
+function showHelp(player)
+    
+    -- <argument> [option]
+    player:SendMessage(cChatColor.Green .. "Usage /home [[name]|set|list|help] [name] ")
+    player:SendMessage(cChatColor.Green .. "/home => port you to your default home")
+    player:SendMessage(cChatColor.Green .. "/home myHome => port you to home 'myHome'")
+    player:SendMessage(cChatColor.Green .. "/home set => set the actual position as default home")
+    player:SendMessage(cChatColor.Green .. "/home set myHome => set the actual position as 'myHome'")
+    player:SendMessage(cChatColor.Green .. "/home list => list all your home by name and world")
+    player:SendMessage(cChatColor.Green .. "/home delete myHome => deletes 'myHome'")
+    player:SendMessage(cChatColor.Green .. "/home help => shows this help")
+    
+    return true
+end
+
+
+-- get config values
+function GetRankLimit(rank)
+
+    if rank == 'VIP' then
+        return g_Config.LIMITVIP
+    elseif rank == 'Operator' then
+        return g_Config.LIMITOP
+    elseif rank == 'Admin' then
+        return g_Config.LIMITADMIN
+    end
+    
+    return g_Config.LIMITUSER
+end
+
+
+-- print tables
+function DebugTable( table )
+    
+    LOG("\n***************** DEBUG START ******************")
+    for key, value in pairs( table ) do
+        LOG("\tKey: " .. key .. "\tValue: " .. value)
+    end
+    LOG("\n***************** DEBUG END ******************")
+end
---
+++ storage.lua
@@ -0,0 +1,186 @@
+
+-- * plugin homes.lua
+
+
+-- * Storage.lua
+-- * seperates the db actions from main plugin
+
+
+-- ********************************************************************
+-- ********************************************************************
+
+
+-- stores class for sqlite access
+cStorage = {};
+
+-- stores an object of the above class for external db access
+g_Storage = {};
+
+
+
+function InitializeStorage()
+    
+    g_Storage = cStorage:new();
+    if (not(g_Storage:OpenDB())) then
+        return false;
+    end
+    return true;
+end
+
+
+function cStorage:new(obj)
+    
+    obj = obj or {};
+    setmetatable(obj, self);
+    self.__index = self;
+    return obj;
+end
+
+
+
+function cStorage:OpenDB()
+    
+    local ErrCode, ErrMsg;
+    self.DB, ErrCode, ErrMsg = sqlite3.open( PluginName .. ".sqlite" );
+    if (self.DB == nil) then
+        LOGWARNING(PluginPrefix .. "Cannot open ProtectionAreas.sqlite, error " .. ErrCode .. " (" .. ErrMsg ..")");
+        return false;
+    end
+    
+    local Homes =
+    {
+        "ID INTEGER PRIMARY KEY AUTOINCREMENT",
+        "Player", 
+        "Name", 
+        "World", 
+        "X", 
+        "Y",
+        "Z",
+    }
+    if ( not(self:CreateTable( PluginName, Homes) )) then
+        LOGWARNING(PluginPrefix .. "Cannot create DB table!");
+        return false;
+    end
+    return true;
+end
+
+
+function cStorage:DBExec(a_SQL, a_Callback, a_CallbackParam)
+    
+    local ErrCode = self.DB:exec(a_SQL, a_Callback, a_CallbackParam);
+    if (ErrCode ~= sqlite3.OK) then
+        LOGWARNING(PluginPrefix .. "Error " .. ErrCode .. " (" .. self.DB:errmsg() ..
+                ") while processing SQL command >>" .. a_SQL .. "<<"
+    );
+        return false;
+    end
+    return true;
+end
+
+
+function cStorage:CreateTable( s_TableName, a_Columns)
+    
+    local sql = "CREATE TABLE IF NOT EXISTS '" .. s_TableName .. "' (" .. table.concat( a_Columns, ", " ) .. ")"
+    if ( not (self:DBExec(sql)) ) then
+        return false
+    end
+    return true
+end
+
+
+function cStorage:CheckForExisting( a_Data )
+    
+    local res = false
+    
+    -- get data from query
+    function getResult(udata, cols, values, names)
+
+        -- for i=1,cols do print('',names[i],values[i]) end
+        if tonumber(values[1]) ~= 0 then res = true end
+        return 0
+    end
+    
+    -- get count of existing homes by Player, Name and World
+    local sql = "SELECT COUNT(*) FROM " .. PluginName .. " WHERE Player='" .. a_Data.PLAYER .. "'"
+    sql = sql .. "AND World='" .. a_Data.WORLD .. "'"
+    sql = sql .. " AND Name='" .. a_Data.NAME .. "'"
+    self:DBExec(sql, getResult) 
+    
+    return res
+end
+
+
+function cStorage:GetTotalCount( a_Data )
+    
+    local count = 0
+
+    -- get data from query
+    function getResult(udata, cols, values, names)
+        
+        count = tonumber(values[1])
+        return 0
+    end
+
+    -- get Count of all Player homes in given world
+    local sql = "SELECT COUNT(*) FROM " .. PluginName .. " WHERE Player='" .. a_Data.PLAYER .. "'"
+    sql = sql .. " AND World='" .. a_Data.WORLD .. "';"
+    self:DBExec(sql, getResult) 
+    return count
+end
+
+
+
+function cStorage:SetHome( a_Data )
+
+    local sql = "INSERT or REPLACE INTO " .. PluginName .. " "
+    sql = sql .. "( ID, Player, Name, World, X, Y, Z ) "
+    sql = sql .. "VALUES ((SELECT ID FROM " .. PluginName .. " WHERE "
+    sql = sql .. "Player='" .. a_Data.PLAYER .. "' AND Name='" .. a_Data.NAME .. "' AND World='" .. a_Data.WORLD .. "' ), "
+    sql = sql .. "'" .. a_Data.PLAYER .. "', '" .. a_Data.NAME .. "', '" .. a_Data.WORLD .. "', "
+    sql = sql .. a_Data.X .. ", " .. a_Data.Y .. ", " .. a_Data.Z .. ");"
+    
+    return self:DBExec(sql)
+end
+
+
+function cStorage:GetHomeList( player )
+    
+    local a_List = {}
+    local sql = "SELECT * FROM " .. PluginName .. " WHERE Player=?"
+    local stmt = self.DB:prepare(sql)
+    stmt:bind(1, player)
+    for rowData in stmt:nrows(sql) do
+        a_List[#a_List+1] = rowData
+    end
+    return a_List
+end
+
+
+function cStorage:GetHome( o_player )
+    
+    local sql = "SELECT * FROM " .. PluginName .. " WHERE Player=? AND Name=?"
+    local stmt = self.DB:prepare(sql)
+    stmt:bind(1, o_player.PLAYER)
+    stmt:bind(2, o_player.NAME)
+    for rowData in stmt:nrows(sql) do
+        return rowData
+    end
+end
+
+
+function cStorage:DeleteHome( o_player )
+    
+    -- delete given player home
+    local sql = "DELETE FROM " .. PluginName .. " WHERE Player=? AND Name=?"
+    local stmt = self.DB:prepare(sql)
+    stmt:bind(1, o_player.PLAYER)
+    stmt:bind(2, o_player.NAME)
+    local ErrCode = stmt:step(sql)
+    if (ErrCode ~= sqlite3.DONE) then
+        return false;
+    end
+    return true;
+end
+
+
+
